98522d2a3e3b869b736a470b0bea6a9368495497
Matthias Pinard <matthias@pixpay.fr>
2024-12-24 14:37:51 +0100

    day 3

--- main.go
+++ main.go
@@ -2,93 +2,115 @@ package main
 
 import (
 	_ "embed"
-	"fmt"
 	"strconv"
 	"strings"
 )
 
-//go:embed input/2.input
+//go:embed input/3.input
 var input string
 
+const (
+	MUL_TOKEN  = "mul"
+	DO_TOKEN   = "do()"
+	DONT_TOKEN = "don't()"
+)
+
+type ParserState uint32
+
+const (
+	TOKEN   ParserState = 0
+	LEFT_P  ParserState = 1
+	A       ParserState = 2
+	COMMA   ParserState = 3
+	B       ParserState = 4
+	RIGHT_P ParserState = 5
+	MUL     ParserState = 6
+	DO      ParserState = 7
+	DONT    ParserState = 8
+)
+
 func main() {
-	l := make([][]int, 0)
-	for _, s := range strings.Split(input, "\n") {
-		v := strings.Split(s, " ")
-		if len(v) == 1 {
-			continue
-		}
-		l1 := make([]int, 0)
-		for _, n := range v {
-			p, _ := strconv.Atoi(n)
-			l1 = append(l1, p)
-		}
-		l = append(l, l1)
-	}
+	var r int64
+	a := ""
+	b := ""
+	pos := 0
 
-	safe := 0
-	for _, r := range l {
-		fmt.Printf("v: %+v\n", r)
-		if checkSafe(r) {
-			safe += 1
-		} else {
-			for i := range len(r) {
-				sr := append([]int{}, r[:i]...)
-				sr = append(sr, r[i+1:]...)
-				if checkSafe(sr) {
-					safe += 1
-					break
-				}
+	s := TOKEN
+
+	t := ""
+	d := true
+
+	for pos != len(input) {
+		c := input[pos]
+		pos += 1
+		if s == TOKEN {
+			t += string(c)
+			if t == MUL_TOKEN {
+				t = ""
+				s = LEFT_P
+			} else if t == DO_TOKEN {
+				d = true
+				t = ""
+				s = TOKEN
+			} else if t == DONT_TOKEN {
+				d = false
+				t = ""
+				s = TOKEN
+			} else if strings.HasPrefix(MUL_TOKEN, t) || strings.HasPrefix(DO_TOKEN, t) || strings.HasPrefix(DONT_TOKEN, t) {
+				// Do nothings
+			} else {
+				t = ""
+				s = TOKEN
 			}
-		}
-	}
-	print(safe)
-}
-
-func checkSafe(r []int) bool {
-	inc := r[0]-r[1] < 0
-	for i := range len(r) - 1 {
-		e := r[i+1] - r[i]
-		if abs(e) < 1 || abs(e) > 3 {
-			return false
-		}
-		if (inc && e < 0) || (!inc && e > 0) {
-			return false
-		}
-	}
-	return true
-}
-
-func abs(v int) int {
-	if v > 0 {
-		return v
-	}
-	return -v
-}
-
-func Part1() {
-	l := make([][]int, 0)
-	for _, s := range strings.Split(input, "\n") {
-		v := strings.Split(s, " ")
-		if len(v) == 1 {
 			continue
 		}
-		l1 := make([]int, 0)
-		for _, n := range v {
-			p, _ := strconv.Atoi(n)
-			l1 = append(l1, p)
-		}
-		l = append(l, l1)
-	}
 
-	safe := 0
-	for _, r := range l {
-		fmt.Printf("v: %+v\n", r)
-		if checkSafe(r) {
-			println("safe")
-			safe += 1
-		} else {
-			println("unsafe")
+		if s == LEFT_P {
+			if c == '(' {
+				s = A
+			} else {
+				s = TOKEN
+			}
+			continue
+		}
+
+		if s == A {
+			if c >= '0' && c <= '9' {
+				a += string(c)
+			} else if c == ',' {
+				s = B
+			} else {
+				a = ""
+				s = TOKEN
+			}
+			continue
+		}
+
+		if s == B {
+			if c >= '0' && c <= '9' {
+				b += string(c)
+			} else if c == ')' {
+				va, err := strconv.ParseInt(a, 10, 64)
+				if err != nil {
+					panic("error parsing a")
+				}
+				vb, err := strconv.ParseInt(b, 10, 64)
+				if err != nil {
+					panic("error parsing b")
+				}
+				if d {
+					r += va * vb
+				}
+				a = ""
+				b = ""
+				s = TOKEN
+			} else {
+				a = ""
+				b = ""
+				s = TOKEN
+			}
+			continue
 		}
 	}
-	print(safe)
+	println(r)
 }
