1f7ff09cc2e6ad7f65f3c707b9e713f967d074d6
Matthias Pinard <matthias@pixpay.fr>
2024-12-25 22:50:28 +0100

    day 3 v2

--- main.go
+++ main.go
@@ -10,107 +10,97 @@ import (
 var input string
 
 const (
-	MUL_TOKEN  = "mul"
+	MUL_TOKEN  = "mul("
 	DO_TOKEN   = "do()"
 	DONT_TOKEN = "don't()"
 )
 
-type ParserState uint32
+type ParserState struct {
+	a   string
+	sum int64
+	do  bool
+}
 
-const (
-	TOKEN   ParserState = 0
-	LEFT_P  ParserState = 1
-	A       ParserState = 2
-	COMMA   ParserState = 3
-	B       ParserState = 4
-	RIGHT_P ParserState = 5
-	MUL     ParserState = 6
-	DO      ParserState = 7
-	DONT    ParserState = 8
-)
+type Parser interface {
+	Feed(c byte, state ParserState) (ParserState, Parser)
+}
 
-func main() {
-	var r int64
-	a := ""
-	b := ""
-	pos := 0
+type Token struct {
+	token string
+}
 
-	s := TOKEN
+type A struct {
+	val string
+}
 
-	t := ""
-	d := true
+type B struct {
+	val string
+}
 
-	for pos != len(input) {
-		c := input[pos]
-		pos += 1
-		if s == TOKEN {
-			t += string(c)
-			if t == MUL_TOKEN {
-				t = ""
-				s = LEFT_P
-			} else if t == DO_TOKEN {
-				d = true
-				t = ""
-				s = TOKEN
-			} else if t == DONT_TOKEN {
-				d = false
-				t = ""
-				s = TOKEN
-			} else if strings.HasPrefix(MUL_TOKEN, t) || strings.HasPrefix(DO_TOKEN, t) || strings.HasPrefix(DONT_TOKEN, t) {
-				// Do nothings
-			} else {
-				t = ""
-				s = TOKEN
-			}
-			continue
+func (token Token) Feed(c byte, state ParserState) (ParserState, Parser) {
+	token.token += string(c)
+	t := token.token
+	if t == MUL_TOKEN {
+		return state, A{}
+	} else if t == DO_TOKEN {
+		state.do = true
+		return state, Token{}
+	} else if t == DONT_TOKEN {
+		state.do = false
+		return state, Token{}
+	} else if strings.HasPrefix(MUL_TOKEN, t) || strings.HasPrefix(DO_TOKEN, t) || strings.HasPrefix(DONT_TOKEN, t) {
+		return state, token
+	} else {
+		return state, Token{}
+	}
+}
+
+func (a A) Feed(c byte, state ParserState) (ParserState, Parser) {
+	if c >= '0' && c <= '9' {
+		a.val += string(c)
+		return state, a
+	} else if c == ',' {
+		state.a = a.val
+		return state, B{}
+	} else {
+		return state, Token{}
+	}
+}
+
+func (b B) Feed(c byte, state ParserState) (ParserState, Parser) {
+	if c >= '0' && c <= '9' {
+		b.val += string(c)
+		return state, b
+	} else if c == ')' {
+		va, err := strconv.ParseInt(state.a, 10, 64)
+		if err != nil {
+			panic("error parsing a")
 		}
-
-		if s == LEFT_P {
-			if c == '(' {
-				s = A
-			} else {
-				s = TOKEN
-			}
-			continue
+		vb, err := strconv.ParseInt(b.val, 10, 64)
+		if err != nil {
+			panic("error parsing b")
 		}
-
-		if s == A {
-			if c >= '0' && c <= '9' {
-				a += string(c)
-			} else if c == ',' {
-				s = B
-			} else {
-				a = ""
-				s = TOKEN
-			}
-			continue
-		}
-
-		if s == B {
-			if c >= '0' && c <= '9' {
-				b += string(c)
-			} else if c == ')' {
-				va, err := strconv.ParseInt(a, 10, 64)
-				if err != nil {
-					panic("error parsing a")
-				}
-				vb, err := strconv.ParseInt(b, 10, 64)
-				if err != nil {
-					panic("error parsing b")
-				}
-				if d {
-					r += va * vb
-				}
-				a = ""
-				b = ""
-				s = TOKEN
-			} else {
-				a = ""
-				b = ""
-				s = TOKEN
-			}
-			continue
+		if state.do {
+			state.sum += va * vb
 		}
 	}
-	println(r)
+	return state, Token{}
+}
+
+func main() {
+	s := ParserState{
+		a:   "",
+		sum: 0,
+		do:  true,
+	}
+	var p Parser
+	p = Token{}
+	pos := 0
+	for pos < len(input) {
+		c := input[pos]
+		print(string(c))
+		s, p = p.Feed(c, s)
+		pos += 1
+	}
+	println(s.sum)
 }
